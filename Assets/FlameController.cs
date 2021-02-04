7a06779c421b65075cfdfb4ca1805d06aaebf85a
pdziki84-student <wasza.dzikosc@gmail.com>
2021-02-04 20:18:02 +0100

    SystemyCzastek

---
+++ Assets/FlameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameController : MonoBehaviour
+{
+    bool play = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            play = !play;
+        
+        if (play)
+            transform.GetComponent<ParticleSystem>().Play(true);
+        else
+            transform.GetComponent<ParticleSystem>().Stop(true);
+    }
+}
